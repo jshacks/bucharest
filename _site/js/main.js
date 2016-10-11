26374cc57438ffc6d31506cc5a729b9de85d7d3a
Antal Andrei <antal.andrei@icloud.com>
2016-10-11 14:04:19 +0300

    Add contacts to slide animation - compiled code

--- _site/js/main.js
+++ _site/js/main.js
@@ -21,7 +21,7 @@ $(document).ready(function(){
 	}
 
 	$('[href="/#challenges"]').click(function(e){
-		doScroll(e, $(this), "#feature")
+		doScroll(e, $(this), "#challenges")
 	});
 	$('[href="/#agenda"]').click(function(e){
 		doScroll(e, $(this), "#agenda")
@@ -34,6 +34,9 @@ $(document).ready(function(){
 	})
 	$('[href="/#sponsors"]').click(function(e){
 		doScroll(e, $(this), "#service")
+	})
+	$('[href="/#contact"]').click(function(e){
+		doScroll(e, $(this), "#register")
 	})
 
 	/*$("#testimonial-slider").owlCarousel({
